54b78c37e245a3d4a108b93c3da1c8e55266828b
tamanjong collins <collinstamanjong21@gmail.com>
2025-03-23 14:58:49 +0100

    week3

--- home.js
+++ home.js
@@ -2,8 +2,21 @@
 const popup = document.getElementById("popupcontainer")
 const getstarted =document.getElementById("getstartedbtn")
 const closebtn = document.getElementById("close")
+const form = document.getElementById("formcontainer")
 console.log(getstarted)
 
 getstarted.addEventListener('click', function(){
     popup.style.display = "flex"
+})
+
+closebtn.addEventListener('click', function(){
+    popup.style.display = "none"
+})
+form.addEventListener('submit', function(){
+    const password = document.getElementById('password').value
+    const confirmPassword = document.getElementById('confirm-password').value
+    if(password !== confirmPassword);{
+        alert("password deos not match")
+        return
+    }
 })
